481c64b3c31017d1ef0abe6146971401497d4e9b
Zainal Arifin <arifin.1602@gmail.com>
2019-06-11 10:29:09 +0700

    first init project podcast ui

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import Vuetify from "vuetify";
-import App from './App.vue'
+import App from './app/App.vue'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
